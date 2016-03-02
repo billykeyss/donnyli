52f1f883651403007f0f3e0051b4f224d6a98121
Billykeyss <billhuang@live.ca>
2016-03-01 23:47:35 -0500

    First Version

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,10 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+function setInput(button) {
+	button.style.display = 'none';
+}
+
 (function($) {
 
 	skel.breakpoints({
